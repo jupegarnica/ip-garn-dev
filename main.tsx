0374b5fb7e4c50ffb8093d8a607e97bb32d21af1
Juan Garnica <juan@harbiz.io>
2024-11-12 16:45:43 +0100

    feat: refactor main component to use HTML template literals and improve IP rendering logic

--- main.tsx
+++ main.tsx
@@ -1,7 +1,6 @@
 import { Context, Hono } from 'hono'
-import { PropsWithChildren } from 'hono/jsx'
 import { getConnInfo } from 'hono/deno'
-import { css } from 'hono/css'
+import { html } from 'hono/html'
 const app = new Hono();
 
 app.get('/', async (c: Context) => {
@@ -9,22 +8,17 @@ app.get('/', async (c: Context) => {
 
   const ip = connInfo?.remote?.address || '';
   console.log(connInfo);
-
-  return c.render(<Ip ip={ip} />);
+  return c.html(Ip(ip));
 });
 
-
-const Main = ({ children }: PropsWithChildren) => (
-  <html>
+const Main = (children: string) => `<html>
     <head>
-      <style dangerouslySetInnerHTML={{
-        __html:
-          `
+      <style>
         :root {
           --glow-shadow-color: #fc06;
         }
         body {
-          margin:0
+          margin: 0;
         }
         a {
           all: unset;
@@ -35,75 +29,60 @@ const Main = ({ children }: PropsWithChildren) => (
           transition: all 0.4s ease;
         }
         a:hover {
-          /* glow effect growing all the screen */
           text-shadow:
-        0 0 10px var(--glow-shadow-color),
-        0 0 20px var(--glow-shadow-color),
-        0 0 30px var(--glow-shadow-color),
-        0 0 40px var(--glow-shadow-color),
-        0 0 50px var(--glow-shadow-color),
-        0 0 60px var(--glow-shadow-color),
-        0 0 70px var(--glow-shadow-color),
-        0 0 80px var(--glow-shadow-color),
-        0 0 90px var(--glow-shadow-color),
-        0 0 100px var(--glow-shadow-color),
-        0 0 110px var(--glow-shadow-color),
-        0 0 120px var(--glow-shadow-color),
-        0 0 130px var(--glow-shadow-color),
-        0 0 140px var(--glow-shadow-color),
-        0 0 150px var(--glow-shadow-color),
-        0 0 160px var(--glow-shadow-color),
-        0 0 170px var(--glow-shadow-color),
-        0 0 250px var(--glow-shadow-color),
-        0 0 300px var(--glow-shadow-color)
-        ;
-
+            0 0 10px var(--glow-shadow-color),
+            0 0 20px var(--glow-shadow-color),
+            0 0 30px var(--glow-shadow-color),
+            0 0 40px var(--glow-shadow-color),
+            0 0 50px var(--glow-shadow-color),
+            0 0 60px var(--glow-shadow-color),
+            0 0 70px var(--glow-shadow-color),
+            0 0 80px var(--glow-shadow-color),
+            0 0 90px var(--glow-shadow-color),
+            0 0 100px var(--glow-shadow-color),
+            0 0 110px var(--glow-shadow-color),
+            0 0 120px var(--glow-shadow-color),
+            0 0 130px var(--glow-shadow-color),
+            0 0 140px var(--glow-shadow-color),
+            0 0 150px var(--glow-shadow-color),
+            0 0 160px var(--glow-shadow-color),
+            0 0 170px var(--glow-shadow-color),
+            0 0 250px var(--glow-shadow-color),
+            0 0 300px var(--glow-shadow-color);
           text-decoration: underline;
-          text-decoration-color: #15d; ;
+          // text-decoration-color: #15d;
           text-decoration-thickness: 0.05em;
           text-underline-offset: 0.1em;
           color: #444;
         }
         body {
-          background:linear-gradient(
-          135deg,
-          rgb(77, 47, 126) 30%,
-          #51327577 100%
+          background: linear-gradient(
+            135deg,
+            rgb(77, 47, 126) 30%,
+            #51327577 100%
           );
           font-size: clamp(20px, 5vmin, 150px);
           background-color: tomato;
-          color:white;
-          height:100vh;
+          color: white;
+          height: 100vh;
           display: flex;
           justify-content: center;
           align-items: center;
-          // font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
-          //   Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
-          //   'Helvetica Neue', sans-serif;
-            font-family: 'Courier New', Courier, monospace;
-      }`
-      }}/>
+          font-family: 'Courier New', Courier, monospace;
+        }
+      </style>
     </head>
     <body>
-      {children}
+      ${children}
     </body>
-  </html >
-);
+  </html>
+`;
 
-function Ip({ ip }: { ip: string }) {
+function Ip(ip: string) {
   if (!ip) {
-    return (
-      <Main>
-        <h1>Could not get your IP</h1>
-      </Main>
-    );
+    return Main('<h1>Could not get your IP</h1>');
   }
-  return (
-    <Main>
-      <a href={`http://${ip}`}>{ip}</a>
-    </Main>
-  );
+  return Main(`<a href="http://${ip}">${ip}</a>`);
 }
-
 
 Deno.serve(app.fetch);
